5fbbf8b68dc59e4781cd12ef9837a63106fac6b2
kimihua1995 <kimihua1995@gmail.com>
2020-11-06 21:24:43 -0500

    Update ps.R
    update ps()

--- R/ps.R
+++ R/ps.R
@@ -2,7 +2,7 @@
 ps <- function(treat,x){
   glm.sol1<-glm((treat)~x, family=binomial(link="logit"))
   beta=glm.sol1$coef
-  pre<-cbind(1,x.new)%*%beta
+  pre<-cbind(1,x)%*%beta
   ex<-(1-1/(1+exp(pre)))
 
   return(ex)
